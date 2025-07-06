5e510b32ead2031bb902756ee70962ce6baa224c
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2025-07-06 01:09:06 -0500

    Create pythonBasic2.py

---
+++ pythonBasic2.py
@@ -0,0 +1,33 @@
+#Functions - Reusable block of code (like a program)
+
+def who_am_i(name,age):
+    print(f"My name is {name} and I am {age} years old")
+
+who_am_i("Heath",35)
+
+def add_one_hundred(num):
+    print(num + 100)
+
+add_one_hundred(100)
+
+def add(x,y):
+    print(x + y)
+
+add(7,7)
+
+def multiply(x,y):
+    return x * y
+
+result1 = multiply(7,7)
+result2 = multiply(8,8)
+print(result1 + result2)
+
+def square_root(x):
+    print(x ** .5)
+
+square_root(64)
+
+def nl(): #new line
+    print('\n')
+
+nl()
